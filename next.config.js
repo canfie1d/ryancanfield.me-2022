b032e29d0babf552183a061cb3c11d1855c5619d
Ryan Canfield <ryancanfield@me.com>
2022-03-28 09:50:04 -0700

    Update config

--- next.config.js
+++ next.config.js
@@ -1,20 +1,24 @@
 const path= require('path');
 
 module.exports = {
+  reactStrictMode: true,
+  modules: true,
+  // experimental: {
+  //   concurrentFeatures: true,
+  //   serverComponents: true,
+  //   runtime: true
+  // },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/styles')],
+  },
   onDemandEntries: {
     // period (in ms) where the server will keep pages in the buffer
     maxInactiveAge: 25 * 1000,
     // number of pages that should be kept simultaneously without being disposed
     pagesBufferLength: 2,
-    sassOptions: {
-      includePaths: [path.join(__dirname, 'styles'),path.join(__dirname, '/src')],
-    },
   },
   images: {
     minimumCacheTTL: 60,
-    domains: [
-      'http://localhost:3000',
-      's3-us-west-2.amazonaws.com'
-    ],
-  },
+    domains: ['s3-us-west-2.amazonaws.com'],
+  }
 }
